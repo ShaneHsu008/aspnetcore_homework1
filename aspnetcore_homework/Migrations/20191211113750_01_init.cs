48aefbc283aae7062c9c126a5bdd083d33da65e0
ShaneHsu <xiaosmall8@gmail.com>
2019-12-11 19:51:58 +0800

    init

---
+++ aspnetcore_homework/Migrations/20191211113750_01_init.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace aspnetcore_homework.Migrations
+{
+    public partial class _01_init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
