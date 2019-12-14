899f6fc5d023147991ebc7ec37a09d27f1f1183c
ShaneHsu <xiaosmall8@gmail.com>
2019-12-14 11:47:06 +0800

    加入swagger

--- aspnetcore_homework/Startup.cs
+++ aspnetcore_homework/Startup.cs
@@ -32,6 +32,7 @@ namespace aspnetcore_homework
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             
             services.AddControllers();
+            services.AddSwaggerDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,11 +43,15 @@ namespace aspnetcore_homework
                 app.UseDeveloperExceptionPage();
             }
 
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
 
             app.UseEndpoints(endpoints =>
             {
